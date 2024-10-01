22eba704f4780cc4f4dbfaf6e3a267c39b6d1c4f
JamieHerron <jamiealex.jh@gmail.com>
2024-10-01 19:28:13 +0800

    Test completed as per odin project specs
    Compared to the solution my code could be cleaner but
    the tests were passed and I understand how each part works
    in both my own code and the solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let printedString = string
+
+    if (num === 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        for (i = 1; i < num; i++) {
+            printedString += string;
+        };
+    
+        return printedString;
+    }
 
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
