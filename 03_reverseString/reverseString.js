8405ac03eac58fb93233f54b5a09ffe11f471edf
JamieHerron <jamiealex.jh@gmail.com>
2024-10-02 13:03:02 +0800

    Javascript corrected to pass TDD
    The function I created seemed to pass all
    tests, I assumed I would need to edit for each test,
    but the initial function covered all requirements

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    let reverseString = "";
 
+    for (i = stringArray.length - 1; i >= 0; i--) {
+        reverseString += stringArray[i];
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
