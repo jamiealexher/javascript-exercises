7b708c958651df308de1020d854c6b4ac9298d71
JamieHerron <jamiealex.jh@gmail.com>
2024-10-03 14:41:17 +0800

    JS edited to pass TDD as per odin project

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...eraseElements) {
+
+let newArray = [];
+
+array.forEach((item) => {
+    if (!eraseElements.includes(item)) {
+        newArray.push(item);
+    }
+});
+
+return newArray;
+
 };
 
 // Do not edit below this line
